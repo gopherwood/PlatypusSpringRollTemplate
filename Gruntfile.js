32ff258f310541433e2c8bb873c35cc89103ed86
Derek Detweiler <derek.detweiler@gopherwoodstudios.com>
2015-07-27 21:35:46 -0400

    Updated Gruntfile with task stubs.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,12 @@
-module.exports = require('grunt-springroll');
+/*global module, require */
+module.exports = function (grunt) {
+    "use strict";
+    
+	require('grunt-springroll')(grunt);
+	
+	// Override-able tasks for adding to the build
+	grunt.registerTask('_pre-build', []);
+	grunt.registerTask('_post-build', []);
+	grunt.registerTask('_pre-build-debug', []);
+	grunt.registerTask('_post-build-debug', []);
+};
