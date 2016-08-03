6c2dfffe76fe60d40d590cdab6afe3f59b1de80d
Derek Detweiler <derek.detweiler@gmail.com>
2016-08-03 15:38:34 -0400

    Updated to Platypus v0.9.0

--- src/main.js
+++ src/main.js
@@ -1,33 +1,19 @@
-/*global include, platypus */
+/*global include, window */
 (function () {
     "use strict";
 
-    // Example of a custom component being created prior to its use by an entity (referenced by its provided id). "createComponentClass" adds the new component to the list of available components.
-    (function () {
-        return platypus.createComponentClass({
-            id: "LogicHero",
-            constructor: function (definition) {
-                
-            },
-            events: {
-                "handle-logic": function () {
-
-                }
-            }
-        });
-    }());
-
-	// Library dependencies
-	var Application = include('springroll.Application'),
-		Display = include('_displayClass_'),
+    // Library dependencies
+    var Application = include('springroll.Application'),
+        Display = include('_displayClass_'),
         app = new Application({ // Create a new application
             name: "_name_",
             canvasId: "stage",
-		    configPath: "assets/config/config.json",
+            configPath: "assets/config/config.json",
             display: Display,
-            displayOptions:	{
+            displayOptions:    {
                 clearView: true
             },
+            enableHiDPI: true,
             preload: [
                 {"id": "buttons",        "src": "assets/images/buttons.png"},
                 {"id": "mookie",         "src": "assets/images/mookie.png"},
@@ -35,14 +21,15 @@
                 {"id": "title-screen",   "src": "assets/images/title-screen.png"}
             ],
             responsive: true,
-            state: "menu"
+            state: "menu",
+            version: "_version_"
         });
 
-	// Handle when app is ready to use
-	app.on('init', function () {
-		// Start game
-	});
+    // Handle when app is ready to use
+    app.on('init', function () {
+        // Start game
+    });
 
-	// Assign to the window for easy access
-	window.app = app;
+    // Assign to the window for easy access
+    window.app = app;
 }());
